8989e73f6fe1b1b5de1c344249e6e9dc02b95ce8
bujueou <113224959+bujueou@users.noreply.github.com>
2022-11-26 20:48:55 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -91,5 +91,22 @@ App({
 //appjs获取openid并作为全局变量使用，没有问题，因为在使用过程中并不需要改变openid
   },
 
- 
+  //获取当前时间,返回时间格式：2018-09-16 15:43:36
+  getNowFormatDate: function () {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
+        " " + date.getHours() + seperator2 + date.getMinutes() +
+        seperator2 + date.getSeconds();
+    return currentdate;
+},
 })
